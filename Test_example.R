91ec25f47fc2a29962c0be52af55ee151ab04df9
Kanishka Narayan <37234086+kanishkan91@users.noreply.github.com>
2020-04-02 20:24:13 -0400

    Create Test_example.R

---
+++ Test_example.R
@@ -0,0 +1,14 @@
+library(COVID19Viualizations)
+
+
+#Load the data
+Cases_Final <- read.csv("./data/Coronavirus_Cases_USA.csv") %>% filter(Day<49)
+
+
+#Generate animation
+g<- generate_COVID_animation(Input_data=Cases_final,Total_ID="USA_total", 
+                             states_selected=c("New York","California",
+                                               "Washington"))
+
+#Animate
+animate(g,nframes = 100,fps=8, width = 1000, height =1000,res=100)
